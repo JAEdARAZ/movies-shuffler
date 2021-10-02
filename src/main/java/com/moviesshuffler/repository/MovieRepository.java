46637e9908ff1f4bc2939876d3cc721c3f181b05
Jaime Aranzazu <jaedaraz@gmail.com>
2021-10-02 16:17:07 +0200

    avoided extra querys when eager fetching movies with user by overriding findAll with Query annotation

--- src/main/java/com/moviesshuffler/repository/MovieRepository.java
+++ src/main/java/com/moviesshuffler/repository/MovieRepository.java
@@ -2,6 +2,7 @@ package com.moviesshuffler.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,9 @@ import com.moviesshuffler.model.Movie;
 
 @Repository
 public interface MovieRepository extends CrudRepository<Movie, Integer>{
+    @Override
+    @Query("select movie from Movie movie left join fetch movie.user")
+    Iterable<Movie> findAll();
+    
     List<Movie> findByUserId(Integer userId);
 }
